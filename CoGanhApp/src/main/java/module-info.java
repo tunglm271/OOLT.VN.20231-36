dee67ac433efdce39c69fcf19c73c8738193d8f0
_vanchieeu <95594868+vanchieeu@users.noreply.github.com>
2023-12-30 22:21:01 +0700

    Organize and update

--- CoGanhApp/src/main/java/module-info.java
+++ CoGanhApp/src/main/java/module-info.java
@@ -5,4 +5,8 @@ module com.example.coganhapp {
 
     opens com.example.coganhapp to javafx.fxml;
     exports com.example.coganhapp;
+    exports com.example.coganhapp.Move;
+    opens com.example.coganhapp.Move to javafx.fxml;
+    exports com.example.coganhapp.Window;
+    opens com.example.coganhapp.Window to javafx.fxml;
 }
